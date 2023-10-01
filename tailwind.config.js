ba210fb20447d65b5889a2a9cba211c183eec7d7
shvam0000 <shvam0000@gmail.com>
2023-09-30 20:44:02 -0400

    🔧 add styles and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
 
     // Or if using `src` directory:
-    './src/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sans-serif': ['Montserrat', 'sans-serif'],
+      },
+      colors: {
+        'primary-orange': '#FF7629',
+        'primary-white': '#FFFFFF',
+        'primary-black': '#000000',
+      },
+    },
   },
   plugins: [],
 };
