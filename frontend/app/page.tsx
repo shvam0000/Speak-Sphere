e8b8a7afd81c81b097a061679d44efe36d2fbf2e
shvam0000 <shvam0000@gmail.com>
2023-11-29 18:28:06 -0500

    🎨 add instructions

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,13 @@
 'use client';
 import { useState } from 'react';
 import { Button } from './components/shared';
-import { Bot, Brain, Learning, Personal } from './utils/icons';
+import { Bot, Brain, Learning, Personal, DownArrow } from './utils/icons';
 import Modal from 'react-lean-modal';
 import { Form } from './components/landing';
+import Image from 'next/image';
+import GetStarted from '@/app/utils/images/get-started.png';
+import Chatbot from '@/app/utils/images/chatbot.png';
+import Translate from '@/app/utils/images/translate.png';
 
 export default function Home() {
   const [showModal, setShowModal] = useState<boolean>(false);
@@ -29,7 +33,7 @@ export default function Home() {
         </figure>
       </div>
       <div className="py-10">
-        <h1 className="font-medium text-4xl flex justify-center pb-5">
+        <h1 className="font-semibold text-4xl flex justify-center pb-5">
           Top Features
         </h1>
         <div className="flex justify-evenly">
@@ -51,6 +55,49 @@ export default function Home() {
             </figure>
             <h1 className="font-normal text-2xl">Personalized Learning</h1>
           </div>
+        </div>
+      </div>
+      <div className="py-5">
+        <h1 className="font-semibold text-4xl flex justify-center">
+          How it Works?
+        </h1>
+        <div className="flex justify-center py-5 pt-10 animate-pulse">
+          <Button handleClick={handleModal} type="primary">
+            <span>Get Started</span>
+          </Button>
+        </div>
+        <div className="flex justify-center items-center py-4">
+          <div className="w-1/2 flex justify-center items-center text-lg font-medium ">
+            Simply Enter your name to get started!
+          </div>
+          <figure className="shadow-lg">
+            <Image src={GetStarted} height={600} width={600} alt="Name Input" />
+          </figure>
+        </div>
+
+        <figure className="flex justify-center items-center text-5xl text-primary-blue animate-bounce py-5">
+          <DownArrow />
+        </figure>
+        <div className="flex justify-center items-center py-4">
+          <figure>
+            <Image src={Chatbot} height={600} width={600} alt="Chatbot" />
+          </figure>
+          <div className="w-1/2 flex justify-center items-center text-lg font-medium text-center px-10">
+            Start talking to the bot and it will respond to you in the spanish
+            trying to teach you more about it.
+          </div>
+        </div>
+        <figure className="flex justify-center items-center text-5xl text-primary-blue animate-bounce py-5">
+          <DownArrow />
+        </figure>
+        <div className="flex justify-center items-center py-4">
+          <div className="w-1/2 flex justify-center items-center text-lg font-medium text-center px-10">
+            Not sure what the reply means? Just hover over the message and see
+            the english translation
+          </div>
+          <figure className="shadow-lg">
+            <Image src={Translate} height={600} width={600} alt="Translate" />
+          </figure>
         </div>
       </div>
       <Modal
