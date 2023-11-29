fe91e8d4812ceb4cee9736036eb6312404221867
shvam0000 <shvam0000@gmail.com>
2023-11-29 18:27:54 -0500

    🍱 add landing page assets

--- frontend/app/utils/icons/index.ts
+++ frontend/app/utils/icons/index.ts
@@ -4,3 +4,4 @@ export { default as Bot } from './bot';
 export { default as Learning } from './learning';
 export { default as Brain } from './brain';
 export { default as Personal } from './personal';
+export { default as DownArrow } from './down-arrow';
