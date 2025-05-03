43ec78b7964eb9c8b218a7a826603c93e1c93309
shvam0000 <shvam0000@gmail.com>
2025-05-03 17:28:54 -0400

    fix: use dynamic port from Heroku

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(cors());
 
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 mongoose.set('strictQuery', false);
 
